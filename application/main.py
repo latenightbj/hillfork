c7880a921902e8d843bc08d731d8776aa40423d3
BJ <bjgodinez+macair@gmail.com>
2021-09-07 00:31:24 -0400

    added jupyter notebook via jupyter lab cleaned up unnecessary columns

--- application/main.py
+++ application/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # read in csv
-z = pd.read_csv('../data/sept20todec2021.csv')
+df = pd.read_csv('../data/sept20todec2021.csv')
 
 # print out the contents of the head
-print(z.head)
+print(z.head())
